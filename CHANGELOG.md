5950f05cbf68bea79420ab0a59ca47cca9f14852
Sumit Arora <er.sumitarora@gmail.com>
2017-06-24 14:12:40 -0400

    chore(release): bump version number to 0.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+<a name="0.3.0"></a>
+# [0.3.0](https://github.com/sumitarora/my-ngx-library/compare/v0.2.0...v0.3.0) (2017-06-24)
+
+
+
 <a name="0.2.0"></a>
 # 0.2.0 (2017-06-24)
 
--- src/my-ng-module.module.ts
+++ src/my-ng-module.module.ts
@@ -1,18 +1,17 @@
-
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { EmptyTextDirective } from './empty-text.directive';
+import { EmptyTextService } from './empty-text.service';
 
 // add your angular components here (directives, components, filters, pipes ...)
-export const MY_NG_COMPONENTS = [];
+export const MY_NG_COMPONENTS = [EmptyTextDirective];
 
 // add your services here
-const MY_SERVICES = [];
+const MY_SERVICES = [EmptyTextService];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   exports: [MY_NG_COMPONENTS],
   declarations: [MY_NG_COMPONENTS]
 })
